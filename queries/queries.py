dcdb5a999a5dd42b4b479e818a685600b5e8f132
Javier Palomares <javier.palomares.90@gmail.com>
2019-11-13 20:04:42 -0600

    Get the wins,era,and losses from the pitcher's records, not the batters

--- queries/queries.py
+++ queries/queries.py
@@ -63,24 +63,27 @@ order by gp.gid, p.tfs_zulu;
 # all pitches for a specific pitcher with batter info
 # 
 PITCHES_WITH_BATTER_INFO_QUERY='''
-select gp.gid, p.tfs_zulu, p.id, p.type, p.pitch_type, p.type_confidence, p.start_speed, p.end_speed, p.spin_rate, p.spin_dir,  
+
+
+
+select pitcher.gid, p.tfs_zulu, p.id, p.type, p.pitch_type, p.type_confidence, p.start_speed, p.end_speed, p.spin_rate, p.spin_dir,  
        p.break_angle, p.break_length, p.break_y, p.x, p.y, p.x0, p.y0, p.z0, p.vx0, p.vy0, p.vz0, p.ax, p.ay, p.az, 
        p.px, p.pz, p.pfx_x, p.pfx_z, p.sz_top, p.sz_bot, p.zone, p.nasty, p.outcome as outcome_pitch, p.game_id,
        ab.pitcher, ab.batter, ab.stand, ab.b_height, ab.b, ab.s, ab.o, ab.outcome as outcome_at_bat, ab.score, 
        ab.home_team_runs, ab.away_team_runs, ab.num as num_at_bat, ab.runner_ids, i.num as num_inning,      
-       gp.id as pitcher_id, gp.boxname as pitcher_boxname, gp.rl as pitcher_rl, gp.bats as pitcher_bats, 
-       gp.era as pitcher_era, gp.wins as pitcher_wins, gp.losses as pitcher_losses, 
-       gp2.id as batter_id, gp2.boxname as batter_boxname, gp2.rl as batter_rl, gp2.bats as batter_bats, 
-       gp2.avg as batter_avg, gp2.hr as batter_hr, gp2.rbi as batter_rbi, gp2.position as batter_position, gp2.bat_order
+       pitcher.id as pitcher_id, pitcher.boxname as pitcher_boxname, pitcher.rl as pitcher_rl, pitcher.bats as pitcher_bats, 
+       pitcher.era as pitcher_era, pitcher.wins as pitcher_wins, pitcher.losses as pitcher_losses, 
+       batter.id as batter_id, batter.boxname as batter_boxname, batter.rl as batter_rl, batter.bats as batter_bats, 
+       batter.avg as batter_avg, batter.hr as batter_hr, batter.rbi as batter_rbi, batter.position as batter_position,  batter.bat_order
 from pitch p
     join game g on p.game_id = g.game_id
     join at_bat ab on p.at_bat_id = ab.at_bat_id
     join inning i on p.inning_id = i.inning_id
-    join game_player gp on ab.pitcher = gp.id
-        and g.gid = gp.gid
-    join game_player gp2 on ab.batter = gp2.id
-        and g.gid = gp2.gid
-where gp.id = '{pitcher_id}'
-order by gp.gid, p.tfs_zulu;
+    join game_player pitcher on ab.pitcher = pitcher.id
+        and g.gid = pitcher.gid
+    join game_player batter on ab.batter = batter.id
+        and g.gid = batter.gid
+where pitcher.id = '{pitcher_id}'
+order by pitcher.gid, p.tfs_zulu;
 '''
 
