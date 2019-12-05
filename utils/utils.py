a670415f1139511f698e0c4126884e1aa1e9bcac
porterperry <porterperry@utexas.edu>
2019-12-04 22:37:07 -0600

    Fixed function call to sklearn train_test_split.

--- utils/utils.py
+++ utils/utils.py
@@ -193,6 +193,6 @@ def load_dataframe(filename):
     df = pd.read_csv(filename)
     return df
 
-def split_dataset_into_train_and_test(X,y,test_size=0.2,random_state=42):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size,random_state)
+def split_dataset_into_train_and_test(X,y,test_sz=0.2,rand_state=42):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_sz, random_state=rand_state)
     return X_train, X_test, y_train, y_test  
