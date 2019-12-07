f73bf4f8cfd05d507c5c6a81a78c9e7d2c7489e8
porterperry <porterperry@utexas.edu>
2019-12-07 15:13:20 -0600

    Added <Unknown> pitch type for encoding, added a function to get the new pitchdata by pitcher query into a dataframe, and added optional <do_shuffle> parameter for splitting test/train data.

--- utils/utils.py
+++ utils/utils.py
@@ -10,8 +10,8 @@ from sklearn.model_selection import train_test_split
 
 
 ENGINE_LOGIN='postgresql+psycopg2://postgres:mlb2018@localhost:5532/' #NOTE for this to work, you need to run the cloud sql proxy using port 5532
-_PITCH_TYPES_ENCODING={'FA':1,'FF':2,'FT':3,'FC':4,'FO':5,'FS':6,'GY':7,'SI':8,'SF':9,'SL':10,'SC':11,'CH':12,'CB':13,'CU':14,'KC':15,'KN':16,'EP':17,'IN':18}
-_PITCH_TYPES_DECODING={1:'FA',2:'FF',3:'FT',4:'FC',5:'FO',6:'FS',7:'GY',8:'SI',9:'SF',10:'SL',11:'SC',12:'CH',13:'CB',14:'CU',15:'KC',16:'KN',17:'EP',18:'IN'}
+_PITCH_TYPES_ENCODING={'FA':1,'FF':2,'FT':3,'FC':4,'FO':5,'FS':6,'GY':7,'SI':8,'SF':9,'SL':10,'SC':11,'CH':12,'CB':13,'CU':14,'KC':15,'KN':16,'EP':17,'IN':18,'Unknown':19}
+_PITCH_TYPES_DECODING={1:'FA',2:'FF',3:'FT',4:'FC',5:'FO',6:'FS',7:'GY',8:'SI',9:'SF',10:'SL',11:'SC',12:'CH',13:'CB',14:'CU',15:'KC',16:'KN',17:'EP',18:'IN',19:'Unknown'}
 
 def get_engine(engine_login):
     engine = create_engine(engine_login)
@@ -52,6 +52,12 @@ def get_pitch_data():
     engine  = get_engine(ENGINE_LOGIN)
     conn = get_connection(engine)
     pitch_data = pd.read_sql(queries.PITCH_DATA_QUERY,conn)
+    return pitch_data
+
+def get_pitch_data_by_pitcher(pitcher_id):
+    engine  = get_engine(ENGINE_LOGIN)
+    conn = get_connection(engine)
+    pitch_data = pd.read_sql(queries.PITCH_DATA_BY_PITCHER_QUERY.format(pitcher_id=pitcher_id),conn)
     return pitch_data
 
 ##
@@ -199,6 +205,6 @@ def drop_pickoffs(df):
     df = df[df.p1_pitch_type != 'PO']
     return df
 
-def split_dataset_into_train_and_test(X,y,test_sz=0.2,rand_state=42):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_sz, random_state=rand_state)
+def split_dataset_into_train_and_test(X,y,test_sz=0.2,rand_state=42, do_shuffle=True):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_sz, random_state=rand_state, shuffle=do_shuffle)
     return X_train, X_test, y_train, y_test  
