9c5e2a91e81e427b69689364945b927f95918ae9
porterperry <porterperry@utexas.edu>
2019-12-08 02:02:39 -0600

    Added an alternate simple pitch type encoding to account for synonymous types. Also added a function definition to drop all unwanted pitch types, included automatic balls/strikes, pitchouts, no pitch, and unknown.

--- utils/utils.py
+++ utils/utils.py
@@ -10,8 +10,29 @@ from sklearn.model_selection import train_test_split
 
 
 ENGINE_LOGIN='postgresql+psycopg2://postgres:mlb2018@localhost:5532/' #NOTE for this to work, you need to run the cloud sql proxy using port 5532
-_PITCH_TYPES_ENCODING={'FA':1,'FF':2,'FT':3,'FC':4,'FO':5,'FS':6,'GY':7,'SI':8,'SF':9,'SL':10,'SC':11,'CH':12,'CB':13,'CU':14,'KC':15,'KN':16,'EP':17,'IN':18,'Unknown':19}
-_PITCH_TYPES_DECODING={1:'FA',2:'FF',3:'FT',4:'FC',5:'FO',6:'FS',7:'GY',8:'SI',9:'SF',10:'SL',11:'SC',12:'CH',13:'CB',14:'CU',15:'KC',16:'KN',17:'EP',18:'IN',19:'Unknown'}
+_PITCH_TYPES_ENCODING={'FA':1,'FF':2,'FT':3,'FC':4,'FO':5,'FS':6,'GY':7,'SI':8,'SF':9,'SL':10,'SC':11,'CH':12,'CB':13,'CU':14,'KC':15,'KN':16,'EP':17,'IN':18,'AB':19,'AS':21,'UN':21,'XX':21,'Unknown':21}
+_PITCH_TYPES_DECODING={1:'FA',2:'FF',3:'FT',4:'FC',5:'FO',6:'FS',7:'GY',8:'SI',9:'SF',10:'SL',11:'SC',12:'CH',13:'CB',14:'CU',15:'KC',16:'KN',17:'EP',18:'IN',19:'AB',20:'AS',21:'UN'}
+
+# Abbreviations and which are synonomous found at:
+#   https://library.fangraphs.com/pitch-type-abbreviations-classifications/
+#   https://www.daktronics.com/support/kb/Pages/DD3312647.aspx
+_PITCH_TYPES_SIMPLE_ENCODING={'FA':1,'FF':1,   #Four-seam fastball
+                              'FT':2,'FS':2,'SI':2,'SF':2, #Two-seam (sinker, split-fingered) fastball
+                              'FC':3, # Fastball cutter
+                              'SL':4, # Slider
+                              'SC':5, # Screwball
+                              'CH':6, # Changeup
+                              'CB':7,'CU':14, # Curveball
+                              'KC':8, # Knuck Curve
+                              'KN':9, # Knuckleball
+                              'FO':10, # Forkball
+                              'EP':11, # Eephus
+                              'GY':12, # Gyroball
+                              'IN':13, 'AB':13, # Intentional/Automatic Ball
+                              'AS':14, #Automatic Strike
+                              'UN':15,'XX':15,'Unknown':15}
+_PITCH_TYPES_SIMPLE_DECODING={1:'FF',2:'FS',3:'FC',4:'SL',5:'SC',6:'CH',7:'CB',8:'KC',9:'KN',10:'FO',11:'EP',12:'GY',13:'IN',14:'AS',15:'UN'}
+
 
 def get_engine(engine_login):
     engine = create_engine(engine_login)
@@ -100,7 +121,7 @@ def get_pitch_encoding(pitch_type):
     return _PITCH_TYPES_ENCODING[pitch_type]
     
 def get_pitch_decoding(num):
-    if pitch_type is None:
+    if num is None:
         return None
     return _PITCH_TYPES_DECODING[num]
 
@@ -108,7 +129,25 @@ def encode_pitch_types(pitch_types):
     return pitch_types.apply(get_pitch_encoding)
 
 def decode_pitch_types(categories):
-    return pitch_types.apply(get_pitch_decoding)
+    return categories.apply(get_pitch_decoding)
+
+
+# Simple version of pitch encoding/decoding
+def get_pitch_simple_encoding(pitch_type):
+    if pitch_type is None:
+        return None
+    return _PITCH_TYPES_SIMPLE_ENCODING[pitch_type]
+
+def get_pitch_simple_decoding(num):
+    if num is None:
+        return None
+    return _PITCH_TYPES_SIMPLE_DECODING[num]
+
+def encode_simple_pitch_types(pitch_types):
+    return pitch_types.apply(get_pitch_simple_encoding)
+
+def decode_simple_pitch_types(categories):
+    return categories.apply(get_pitch_simple_decoding)
 
 
 default_pd_dtype_map_dict = {
@@ -203,6 +242,29 @@ def drop_pickoffs(df):
     #pickoffs have pitch_type 'PO'
     df = df[df.p0_pitch_type != 'PO']
     df = df[df.p1_pitch_type != 'PO']
+    return df
+
+def drop_unwanted_pitches(df):
+    # Drop pitchouts, which have pitch_type 'PO' or 'FO'
+    df = df[df.p0_pitch_type != 'PO']
+    df = df[df.p1_pitch_type != 'PO']
+    df = df[df.p0_pitch_type != 'FO']
+    df = df[df.p1_pitch_type != 'FO']
+    # Drop automatic/intentional balls, which have pitch_type 'AB' or 'IN'
+    df = df[df.p0_pitch_type != 'AB']
+    df = df[df.p1_pitch_type != 'AB']
+    df = df[df.p0_pitch_type != 'IN']
+    df = df[df.p1_pitch_type != 'IN']
+    # Drop automatic strikes, which have type 'AS'
+    df = df[df.p0_pitch_type != 'AS']
+    df = df[df.p1_pitch_type != 'AS']
+    # Drop "no pitch", which have type 'NP'
+    df = df[df.p0_pitch_type != 'NP']
+    df = df[df.p1_pitch_type != 'NP']
+    # Drop unknown pitch type for p1 (to be predicted) only.
+    df = df[df.p1_pitch_type != 'UN']
+    df = df[df.p1_pitch_type != 'XX']
+    df = df[df.p1_pitch_type != 'Unknown']
     return df
 
 def split_dataset_into_train_and_test(X,y,test_sz=0.2,rand_state=42, do_shuffle=True):
