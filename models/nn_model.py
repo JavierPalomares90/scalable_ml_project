b6c5ee0e5ef79db866bdd9dc65b505de35be1792
Javier Palomares <javier.palomares.90@gmail.com>
2019-12-08 20:23:22 -0600

    Add neural network. Need to optimize it

--- models/nn_model.py
+++ models/nn_model.py
@@ -14,12 +14,12 @@ from sklearn.pipeline import Pipeline
 def get_multi_class_classifier_model(num_inputs,num_outputs):
     # create model
     model = Sequential()
-    # Dense(64) is a fully-connected layer with 64 hidden units.
-    # in the first layer, you must specify the expected input data shape:
-    model.add(Dense(128, activation='relu',input_shape=(num_inputs,)))
-    model.add(Dropout(0.5))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.5))
+    # Fully connected layers with 256 nodes.
+    model.add(Dense(256, activation='relu',input_shape=(num_inputs,)))
+    # Dropout of .5 to prevent overfitting
+    model.add(Dropout(0.1))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.1))
     model.add(Dense(num_outputs, activation='softmax'))
     # Compile model
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
@@ -29,10 +29,8 @@ def get_multi_class_classifier_model(num_inputs,num_outputs):
 
     return model
 
-def fit_multi_class_model(model,x_train,y_train,x_test,y_test,num_epochs=20,batch_sz=128,set_verbose=0,num_splits=10,set_shuffle=True):
-    #estimator =  KerasClassifier(model,epochs=num_epochs,batch_size=batch_sz,verbose=set_verbose)
-    #kfold = KFold(n_splits=num_splits,shuffle=set_shuffle)
-    #results = cross_val_score(estimator,X,y,cv=kfold)
+def fit_multi_class_model(model,x_train,y_train,x_test,y_test,num_epochs=200,batch_sz=1024,set_verbose=0,num_splits=10,set_shuffle=True):
+
     model.fit(x_train,y_train,epochs = num_epochs, batch_size = batch_sz)
     score = model.evaluate(x_test,y_test,batch_size=batch_sz)
     return score
