565aa2556b93ddb5288797fd6b11c3d01f9b2063
Javier Palomares <javier_palomares@homedepot.com>
2019-12-11 14:30:16 -0600

    Fix one more bug

--- train_nn.py
+++ train_nn.py
@@ -146,10 +146,10 @@ def main():
     pd_train_scherzer,pd_test_scherzer = drop_season_pitch_id_cols(pd_train_scherzer,pd_test_scherzer)
     pd_train_porcello,pd_test_porcello = drop_season_pitch_id_cols(pd_train_porcello,pd_test_porcello)
 
+    num_pitch_types = 16
     # get the NN model for Verlander
     X_test_verlander,Y_test_verlander = get_X_Y(pd_test_verlander,num_pitch_types)
     X_train_verlander,Y_train_verlander = get_X_Y(pd_train_verlander,num_pitch_types)
-    num_pitch_types = 16
     num_cols = len(X_test_verlander.iloc[0,:])
 
     # train the model for verlander
