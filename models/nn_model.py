42ec8905932c620be069c3f2d466c82bd0093ba5
Porter Perry <porterperry@utexas.edu>
2019-12-15 19:16:00 -0500

    Update NN model to optionally use class weights computed using sklearn utility.

--- models/nn_model.py
+++ models/nn_model.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas
 import keras
 from keras.models import Sequential
@@ -13,6 +14,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
+from sklearn.utils import class_weight
 
 
 def get_multi_class_classifier_model(num_inputs,num_outputs):
@@ -34,9 +36,22 @@ def get_multi_class_classifier_model(num_inputs,num_outputs):
 
     return model
 
-def fit_multi_class_model(model,x_train,y_train,x_test,y_test,num_epochs=200,batch_sz=1024,set_verbose=0,num_splits=10,set_shuffle=True,save_location=None):
+def fit_multi_class_model(model,x_train,y_train,x_test,y_test,num_epochs=200,batch_sz=1024,set_verbose=0,num_splits=10,set_shuffle=True,weight_classes=False,save_location=None):
 
-    model.fit(x_train,y_train,epochs = num_epochs, batch_size = batch_sz)
+    if weight_classes:
+        y_train_labels = np.argmax(y_train, axis=1) 
+        weights = class_weight.compute_class_weight('balanced', np.unique(y_train_labels), y_train_labels)
+
+        model.fit(x_train,y_train,
+                epochs = num_epochs, 
+                batch_size = batch_sz, 
+                class_weight = weights,
+                validation_split = 0.25)
+    else:
+        model.fit(x_train,y_train,
+                epochs = num_epochs, 
+                batch_size = batch_sz) 
+
     score = model.evaluate(x_test,y_test,batch_size=batch_sz)
     if(save_location is not None):
         model.save(save_location)
